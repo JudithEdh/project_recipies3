9f8c2b693e037d82037cba43ede369f099b234b5
Judith E <judith.edhogbo@gmail.com>
2020-08-26 16:40:44 +0100

    first commit

---
+++ src/main/java/com/qa/project_recipies/ProjectRecipiesApplication.java
@@ -0,0 +1,13 @@
+package com.qa.project_recipies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRecipiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectRecipiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qa/project_recipies/ProjectRecipiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.project_recipies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRecipiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
