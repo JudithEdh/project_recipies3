e42c3f51b8bbc2c0382a00d81ef687cfcf05571b
Judith E <judith.edhogbo@gmail.com>
2020-08-27 14:53:20 +0100

    created recipes service

--- src/main/java/com/qa/project_recipies/service/RecipesService.java
+++ src/main/java/com/qa/project_recipies/service/RecipesService.java
@@ -1,4 +1,35 @@
 package com.qa.project_recipies.service;
 
+import com.qa.project_recipies.domain.Recipe;
+import com.qa.project_recipies.exceptions.RecipeNotFoundException;
+import com.qa.project_recipies.repo.RecipesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RecipesService {
+    private final RecipesRepository repo;
+
+    @Autowired
+    public RecipesService(RecipesRepository repo){this.repo = repo;}
+
+    public List<Recipe> readAllRecipes(){return this.repo.findAll();}
+    public Recipe createRecipe(Recipe recipe){ return this.repo.save(recipe);}
+    public Recipe findRecipeById(Long id){return this.repo.findById(id).orElseThrow(RecipeNotFoundException::new);}
+    public Recipe updateRecipe(Long id, Recipe recipe){
+        Recipe update=findRecipeById(id);
+        update.setName(recipe.getName());
+        update.setProcedure(recipe.getProcedure());
+        return this.repo.save(update);
+    }
+    public Boolean deleteRecipesById(Long id){
+        if(!this.repo.existsById(id)){
+            throw new RecipeNotFoundException();
+        }
+
+        this.repo.deleteById(id);
+        return this.repo.existsById(id);
+    }
 }
