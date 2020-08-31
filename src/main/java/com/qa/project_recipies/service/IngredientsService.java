ede979a63a7c494f97472d4b358e72eb739833aa
Judith E <judith.edhogbo@gmail.com>
2020-08-31 16:02:53 +0100

    updated Ingredient Service to return dtos

--- src/main/java/com/qa/project_recipies/service/IngredientsService.java
+++ src/main/java/com/qa/project_recipies/service/IngredientsService.java
@@ -1,30 +1,48 @@
 package com.qa.project_recipies.service;
 
 import com.qa.project_recipies.domain.Ingredient;
+import com.qa.project_recipies.dto.IngredientDTO;
 import com.qa.project_recipies.repo.IngredientsRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.qa.project_recipies.exceptions.IngredientNotFoundException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class IngredientsService {
     private final IngredientsRepository repo;
+    private final ModelMapper mapper;
 
     @Autowired
-    public IngredientsService(IngredientsRepository repo){this.repo = repo;}
+    public IngredientsService(IngredientsRepository repo, ModelMapper mapper){
+        this.repo = repo;
+        this.mapper = mapper;
+    }
 
-    public List<Ingredient> readAllIngredients(){return this.repo.findAll();}
+    private IngredientDTO mapToDTO(Ingredient ingredient){
+        return this.mapper.map(ingredient, IngredientDTO.class);
+    }
 
-    public Ingredient createIngredient(Ingredient ingredient){ return this.repo.save(ingredient);}
-    public Ingredient findIngredientById(Long id){return this.repo.findById(id).orElseThrow(IngredientNotFoundException::new);}
+    public List<IngredientDTO> readAllIngredients(){
+        return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+    }
 
-    public Ingredient updateIngredient(Long id, Ingredient ingredient){
-        Ingredient update=findIngredientById(id);
+    public IngredientDTO createIngredient(Ingredient ingredient){
+        return this.mapToDTO(this.repo.save(ingredient));
+    }
+
+    public IngredientDTO findIngredientById(Long id){
+        return this.mapToDTO(this.repo.findById(id).orElseThrow(IngredientNotFoundException::new));
+    }
+
+    public IngredientDTO updateIngredient(Long id, Ingredient ingredient){
+        Ingredient update=this.repo.findById(id).orElseThrow(IngredientNotFoundException::new);
         update.setIngredient(ingredient.getIngredient());
         update.setQuantity(ingredient.getQuantity());
-        return this.repo.save(update);
+        return this.mapToDTO(this.repo.save(update));
     }
     public Boolean deleteIngredientsById(Long id){
         if(!this.repo.existsById(id)){
