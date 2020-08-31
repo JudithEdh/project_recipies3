0157e0aa71b68ed1811a5b72aef4e4a8c57ed57f
Judith E <judith.edhogbo@gmail.com>
2020-08-31 15:44:25 +0100

    updated Recipe Service to return dtos

--- src/main/java/com/qa/project_recipies/service/RecipesService.java
+++ src/main/java/com/qa/project_recipies/service/RecipesService.java
@@ -1,28 +1,47 @@
 package com.qa.project_recipies.service;
 
 import com.qa.project_recipies.domain.Recipe;
+import com.qa.project_recipies.dto.RecipeDTO;
 import com.qa.project_recipies.exceptions.RecipeNotFoundException;
 import com.qa.project_recipies.repo.RecipesRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RecipesService {
     private final RecipesRepository repo;
 
-    @Autowired
-    public RecipesService(RecipesRepository repo){this.repo = repo;}
+    private final ModelMapper mapper;
 
-    public List<Recipe> readAllRecipes(){return this.repo.findAll();}
-    public Recipe createRecipe(Recipe recipe){ return this.repo.save(recipe);}
-    public Recipe findRecipeById(Long id){return this.repo.findById(id).orElseThrow(RecipeNotFoundException::new);}
-    public Recipe updateRecipe(Long id, Recipe recipe){
-        Recipe update=findRecipeById(id);
+    @Autowired
+    public RecipesService(RecipesRepository repo, ModelMapper mapper){
+        this.repo = repo;
+        this.mapper = mapper;
+    }
+    private RecipeDTO mapToDTO(Recipe recipe){
+        return this.mapper.map(recipe, RecipeDTO.class);
+    }
+    public List<RecipeDTO> readAllRecipes(){
+        return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+    }
+
+    public RecipeDTO createRecipe(Recipe recipe){
+        return this.mapToDTO(this.repo.save(recipe));
+    }
+
+    public RecipeDTO findRecipeById(Long id){
+        return this.mapToDTO(this.repo.findById(id).orElseThrow(RecipeNotFoundException::new));
+    }
+
+    public RecipeDTO updateRecipe(Long id, Recipe recipe){
+        Recipe update=this.repo.findById(id).orElseThrow(RecipeNotFoundException::new);
         update.setName(recipe.getName());
         update.setProcedure(recipe.getProcedure());
-        return this.repo.save(update);
+        return this.mapToDTO(repo.save(update));
     }
     public Boolean deleteRecipesById(Long id){
         if(!this.repo.existsById(id)){
