10c868c10905d4fc0405e5827149107c31d8e39f
Judith E <judith.edhogbo@gmail.com>
2020-08-31 13:33:38 +0100

    completed recipe dto

--- src/main/java/com/qa/project_recipies/dto/RecipeDTO.java
+++ src/main/java/com/qa/project_recipies/dto/RecipeDTO.java
@@ -1,4 +1,39 @@
 package com.qa.project_recipies.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecipeDTO {
+
+    private Long id;
+    private String name;
+    private String procedure;
+    private List<IngredientDTO> ingredients= new ArrayList<>();
+
+    public RecipeDTO(){}
+    public RecipeDTO(String name){
+        super();
+        this.name=name;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<IngredientDTO> getIngredients(){
+        return ingredients;
+    }
+    public void setIngredients(List<IngredientDTO> ingredients){
+        this.ingredients=ingredients;
+    }
 }
